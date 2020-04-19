bcfbe6159137bf4336231952fd251c43b48bc754
Jace Browning <jace.browning@zapier.com>
2020-04-19 14:13:55 -0400

    Prompt for credentials

--- amazon/__init__.py
+++ amazon/__init__.py
@@ -1,22 +1,34 @@
-from pomace import visit, autopage, shared
+from pomace import visit, get_secret
+import log
 
 
-def reload_balance(email: str, password: str, amount: str, _repeat: str):
+def reload_balance(amount: str, _repeat: str):
     repeat = int(_repeat)
 
     page = visit("https://www.amazon.com/asv/reload/order", browser="Firefox")
 
+    email = get_secret("email")
+    password = get_secret("password")
+    card = get_secret("card")
+    assert email and password and card
+
+    log.info(f"Logging in as {email}")
     page = page.click_sign_in().fill_email(email).click_continue()
     page = page.fill_password(password).click_sign_in()
 
-    for x in range(repeat):
+    for index in range(repeat):
+        log.info(f"Selecting amount ${amount}")
         page = page.fill_amount(amount)
 
-        input()
-        page = page.click_reload()
+        assert f"Reload ${amount}" in page
+        log.info("Reloading balance")
+        page = page.click_reload(delay=2)
 
-        if "Verify your card" in shared.browser.html:
-            input()
+        if "Verify your card" in page:
+            log.info("Handling card verification")
+            page = page.fill_card(card).click_verify_card()
+            assert f"Reload ${amount}" in page
+            page = page.click_reload(delay=2)
 
-        page.click_reload_again()
-
+        log.info(f"Balance reloaded {index + 1} time(s)")
+        page = page.click_reload_again()
--- amazon/__main__.py
+++ amazon/__main__.py
@@ -1,7 +1,14 @@
 import sys
+import log
 
 from . import reload_balance
 
 
 if __name__ == "__main__":
-    reload_balance(*sys.argv[1:])
+    try:
+        reload_balance(*sys.argv[1:])
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        log.exception(e)
+        breakpoint()
