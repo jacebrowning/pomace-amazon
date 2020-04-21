5a422d8c38d59b5bad02d753505c22d580e5ebfc
Jace Browning <jace.browning@zapier.com>
2020-04-20 20:16:30 -0400

    Add CLI

--- amazon/__init__.py
+++ amazon/__init__.py
@@ -1,22 +1,16 @@
-from pomace import visit, get_secret
+import pomace
 import log
 
 
-def reload_balance(amount: str, _repeat: str):
-    repeat = int(_repeat)
+def reload_balance(amount: str, repeat: str):
+    page = pomace.visit("https://www.amazon.com/asv/reload/order")
+    settings = pomace.prompt("email", "password", "card")
 
-    page = visit("https://www.amazon.com/asv/reload/order", browser="Firefox")
+    log.info(f"Logging in as {settings.email}")
+    page = page.click_sign_in().fill_email(settings.email).click_continue()
+    page = page.fill_password(settings.password).click_sign_in()
 
-    email = get_secret("email")
-    password = get_secret("password")
-    card = get_secret("card")
-    assert email and password and card
-
-    log.info(f"Logging in as {email}")
-    page = page.click_sign_in().fill_email(email).click_continue()
-    page = page.fill_password(password).click_sign_in()
-
-    for index in range(repeat):
+    for index in range(int(repeat)):
         log.info(f"Selecting amount ${amount}")
         page = page.fill_amount(amount)
 
@@ -26,7 +20,7 @@ def reload_balance(amount: str, _repeat: str):
 
         if "Verify your card" in page:
             log.info("Handling card verification")
-            page = page.fill_card(card).click_verify_card()
+            page = page.fill_card(settings.card).click_verify_card()
             assert f"Reload ${amount}" in page
             page = page.click_reload(delay=2)
 
